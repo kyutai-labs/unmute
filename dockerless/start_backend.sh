cb3233c3db8a6ab619ea9587450987d1068e423b
Traun Leyden <traun.leyden@gmail.com>
2025-10-31 11:33:34 +0000

    local runpod changes

--- dockerless/start_backend.sh
+++ dockerless/start_backend.sh
@@ -2,4 +2,6 @@
 set -ex
 cd "$(dirname "$0")/.."
 
+export UNMUTE_CORS_ALLOW_ORIGINS="https://xzso00wrvev0el-3000.proxy.runpod.net/"
+
 uv run uvicorn unmute.main_websocket:app --reload --host 0.0.0.0 --port 8000 --ws-per-message-deflate=false
--- dockerless/start_frontend.sh
+++ dockerless/start_frontend.sh
@@ -2,7 +2,10 @@
 set -ex
 cd "$(dirname "$0")/.."
 
+export NEXT_PUBLIC_BACKEND_SERVER_URL="https://xzso00wrvev0el-8000.proxy.runpod.net/"
+
 cd frontend
 pnpm install
 pnpm env use --global lts
 pnpm dev
+# pnpm run dev -- -H 0.0.0.0 -p 3000
