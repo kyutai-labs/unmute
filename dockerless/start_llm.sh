47a838ea5cbb7d5823de23ff71caa4bba961b69e
Traun Leyden <traun.leyden@gmail.com>
2025-11-03 11:11:45 +0100

    Use more recent version of vllm (#150)
    * Use more recent version of vllm
    
    * bump version everywhere

--- dockerless/start_llm.sh
+++ dockerless/start_llm.sh
@@ -2,7 +2,7 @@
 set -ex
 cd "$(dirname "$0")/.."
 
-uv tool run vllm@v0.9.1 serve \
+uv tool run vllm@v0.11.0 serve \
   --model=google/gemma-3-1b-it \
   --max-model-len=8192 \
   --dtype=bfloat16 \
