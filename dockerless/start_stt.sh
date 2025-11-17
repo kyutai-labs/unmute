eb0c80255d226e734eaca5c8e143f0c1052a801d
Traun Leyden <traun.leyden@gmail.com>
2025-11-17 18:15:39 +0100

    Refactor comments in start_stt.sh for clarity
    Updated comments for clarity on Python dependencies and environment setup.

--- dockerless/start_stt.sh
+++ dockerless/start_stt.sh
@@ -2,17 +2,12 @@
 set -ex
 cd "$(dirname "$0")/.."
 
-# This is part of a hack to get dependencies needed for the TTS Rust server, because it integrates a Python component
-[ -f pyproject.toml ] || wget https://raw.githubusercontent.com/kyutai-labs/moshi/9837ca328d58deef5d7a4fe95a0fb49c902ec0ae/rust/moshi-server/pyproject.toml
-[ -f uv.lock ] || wget https://raw.githubusercontent.com/kyutai-labs/moshi/9837ca328d58deef5d7a4fe95a0fb49c902ec0ae/rust/moshi-server/uv.lock
 
+# We need libpython because the TTS uses a Python component. STT and TTS have the same executable, so we need
+# to have libpython even if we don't end up using it. For simplicity, we use the same code as for TTS, even though
+# you don't need to install any of these Python packages if you're only using the STT.
 uv venv
 source .venv/bin/activate
-
-# This env var must be set to get the correct environment for the Rust build.
-# Must be set before running `cargo install`!
-# If you don't have it, you'll see an error like `no module named 'huggingface_hub'`
-# or similar, which means you don't have the necessary Python packages installed.
 export LD_LIBRARY_PATH=$(python -c 'import sysconfig; print(sysconfig.get_config_var("LIBDIR"))')
 
 # A fix for building Sentencepiece on GCC 15, see: https://github.com/google/sentencepiece/issues/1108
