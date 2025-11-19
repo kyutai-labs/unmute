6ef7869c9f7f17218a6a1f7038f5ac289b1c16cd
Traun Leyden <traun.leyden@gmail.com>
2025-11-19 13:00:11 +0100

    Update start_stt.sh to match start_tts.sh (#149)
    * Update start_stt.sh to match start_tts.sh
    
    * Refactor comments in start_stt.sh for clarity
    
    Updated comments for clarity on Python dependencies and environment setup.

--- dockerless/start_stt.sh
+++ dockerless/start_stt.sh
@@ -2,6 +2,14 @@
 set -ex
 cd "$(dirname "$0")/.."
 
+
+# We need libpython because the TTS uses a Python component. STT and TTS have the same executable, so we need
+# to have libpython even if we don't end up using it. For simplicity, we use the same code as for TTS, even though
+# you don't need to install any of these Python packages if you're only using the STT.
+uv venv
+source .venv/bin/activate
+export LD_LIBRARY_PATH=$(python -c 'import sysconfig; print(sysconfig.get_config_var("LIBDIR"))')
+
 # A fix for building Sentencepiece on GCC 15, see: https://github.com/google/sentencepiece/issues/1108
 export CXXFLAGS="-include cstdint"
 
