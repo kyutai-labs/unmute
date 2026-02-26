0dc510492ce28b011caac65657ce27d7a81e065c
Václav Volhejn <vaclav@kyutai.org>
2026-02-26 18:34:27 +0100

    Prepare for downscaling the prod instance

--- bake_deploy_staging.sh
+++ bake_deploy_staging.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-set -ex
+set -e # Exit on error
 
 uv run unmute/scripts/check_hugging_face_token_not_write.py $HUGGING_FACE_HUB_TOKEN
 
-export DOMAIN=unmute-staging.kyutai.io
-export KYUTAI_LLM_MODEL=google/gemma-3-4b-it
+set -x # Print commands
+
+export DOMAIN=unmute.kyutai.org
+export KYUTAI_LLM_MODEL=google/gemma-3-12b-it
 export DOCKER_HOST=ssh://root@${DOMAIN}
 
 echo "If you get an connection error, do: ssh root@${DOMAIN}"
+
+export KYUTAI_LLM_API_KEY=$OPENROUTER_API_KEY_UNMUTE
 
 docker buildx bake -f ./swarm-deploy.yml --allow=ssh --push
 docker stack deploy --with-registry-auth --prune --compose-file ./swarm-deploy.yml llm-wrapper
